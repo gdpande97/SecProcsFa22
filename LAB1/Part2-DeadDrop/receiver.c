46cd8b0adace6dbb665c6548822b85e9ea736858
A59005457 <A59005457@eng.ucsd.edu>
2022-10-20 21:13:28 -0700

    Added sender and receiver code for a prime+probe covert channel

--- LAB1/Part2-DeadDrop/receiver.c
+++ LAB1/Part2-DeadDrop/receiver.c
@@ -2,24 +2,94 @@
 #include"util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
+#include <time.h>
+
+uint64_t get_set_index(uint64_t addr) {
+  uint64_t lower_bit_mask = (1<<15) - 1;
+  return (addr & lower_bit_mask)>>6;
+}
 
 int main(int argc, char **argv)
 {
 	// Put your covert channel setup code here
 
-	printf("Please press enter.\n");
+	int sets_to_watch[8] = {10, 45, 121, 180, 235, 312, 397, 447};
+  	uint64_t eviction_set[8][8];
 
-	char text_buf[2];
-	fgets(text_buf, sizeof(text_buf), stdin);
 
-	printf("Receiver now listening.\n");
+  	int assoc = 8;
+ 	int bl_offset_size = 64;
+	int bl_offset_bits = 6;
+	int set_ind_bits = 9;
+	int num_sets = 512;
 
+
+	char* buffer = (char *)malloc(262144*sizeof(char));
+
+	uint64_t start_add = (uint64_t) buffer;
+	printf("Finding eviction set addresses\n");
+	printf("Buffer start address is 0x%lx\n", start_add);
+	for(int set_id=0; set_id<8; set_id++) {
+    		int address_count = 0;
+		printf("set id = %d\n", set_id);
+    		for(int set_index = 0; set_index < 512; set_index++) {
+        		for(int way_index = 0; way_index < assoc; way_index++) {
+				//printf("set index = %d, way_index = %d", set_index, way_index);
+            			uint64_t  address = (uint64_t) (buffer + (set_index * assoc * bl_offset_size) + (way_index* bl_offset_size));
+            			if(get_set_index(address) == sets_to_watch[set_id]) {
+                			eviction_set[set_id][address_count] = address;
+                			address_count++;
+            			}
+        		}
+    		}
+	}
+	clock_t start, curr;
+
+
+	printf("Priming cache now\n");
+	for(int reps = 0; reps <5000; reps++) {
+		for(int itr = 0; itr < 8; itr++) {
+			for(int way_num = 0; way_num < assoc; way_num++) {
+				uint64_t *temp_ptr = eviction_set[itr][way_num];
+				uint64_t temp = *temp_ptr;
+			}
+		}
+	}
+
+	printf("prime is done\n");
+
+	start = clock();
+
+	curr = start;
+
+	while(curr - start <10000000) {
+		curr = clock();
+	}
+
+	while(curr - start <10000000) {
+		curr = clock();
+	}
+//	printf("Please press enter.\n");
+
+//	char text_buf[2];
+//	fgets(text_buf, sizeof(text_buf), stdin);
+
+//	printf("Receiver now listening.\n");
+	
 	bool listening = true;
 	while (listening) {
-
+		printf("Probing the cache accesses now\n");
 		// Put your covert channel code here
-
+		for(int itr=0; itr < 8; itr++) {
+			for(int way_num = 0; way_num < assoc; way_num++) {
+				int time = measure_one_block_access_time(eviction_set[itr][way_num]);
+				printf("Address: 0x%lx, way: %d, set: %d, time required: %d\n", eviction_set[itr][way_num], way_num, sets_to_watch[itr], time); 
+			}
+		}	
+		printf("Probing finished\n");
+		listening = false;
 	}
+
 
 	printf("Receiver finished.\n");
 
--- LAB1/Part2-DeadDrop/sender.c
+++ LAB1/Part2-DeadDrop/sender.c
@@ -7,6 +7,11 @@
 #define BUFF_SIZE (1<<21)
 //#define BUFF_SIZE [TODO]
 
+uint64_t get_set_index(uint64_t addr) {
+  uint64_t lower_bit_mask = (1<<15) - 1;
+  return (addr & lower_bit_mask)>>6;
+}
+
 int main(int argc, char **argv)
 {
   // Allocate a buffer using huge page
@@ -22,10 +27,39 @@ int main(int argc, char **argv)
   // Thus, we use a dummy write here to trigger page allocation
   // so later access will not suffer from such overhead.
   //*((char *)buf) = 1; // dummy write to trigger page allocation
+  
 
+  int sets_to_watch[8] = {10, 45, 121, 180, 235, 312, 397, 447};
+  uint64_t eviction_set[8][8];
 
+  
+  int assoc = 8;
+  int bl_offset_size = 64;
+  int bl_offset_bits = 6;
+  int set_ind_bits = 9;
+  int num_sets = 512;
+
+  char* buffer = (char *)malloc(262144*sizeof(char));
+
+  uint64_t start_add = (uint64_t) buffer;
+  printf("Buffer start address is 0x%lx\n", start_add);
+  for(int set_id=0; set_id<8; set_id++) {
+      int address_count = 0;
+      for(int set_index = 0; set_index < 512; set_index++) {
+	  for(int way_index = 0; way_index < assoc; way_index++) {
+	      uint64_t  address = (uint64_t) (buffer + (set_index * assoc * bl_offset_size) + (way_index* bl_offset_size));
+	      if(get_set_index(address) == sets_to_watch[set_id]) {
+		  eviction_set[set_id][address_count] = address;
+		  address_count++;
+	      }
+	  }
+      }
+  }
+
+  printf("Buffer is allocated %c\n", *((char *)buf)); 
   // TODO:
   // Put your covert channel setup code here
+
 
   printf("Please type a message.\n");
 
@@ -33,12 +67,40 @@ int main(int argc, char **argv)
   while (sending) {
       char text_buf[128];
       fgets(text_buf, sizeof(text_buf), stdin);
-
+      sending=false;
       // TODO:
       // Put your covert channel code here
+      char *msg = string_to_binary(text_buf);
+      printf("MSG is %s\n",msg);
+      char *debug_msg = binary_to_string(msg);
+      printf("Debug msg is %s\n", debug_msg);
+      //int binary_msg[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+      int i = 0;
+//     while(msg>0) {
+//	binary_msg[i] = msg%2;
+//	msg=msg/2;
+//	i++;
+//      }
+
+
+      int len = 8;
+      for(int j = 0; j < len; j++) {
+	 if(msg[j] == '1'){
+	    printf("Sender is evicting for set[%d]\n",sets_to_watch[j]);
+	    for(int reps= 0; reps < 10000; reps++) {
+		for(int way_num = 0; way_num < assoc; way_num++) {
+	    	    uint64_t *ptr = (uint64_t *) eviction_set[j][way_num];
+		    volatile char tmp = *ptr;
+		}
+	    }  
+	 }
+	 else {;}
+      }
   }
 
   printf("Sender finished.\n");
+
+  free(buffer);
   return 0;
 }
 
